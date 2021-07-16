9334a1914f35b9ca4b878028b2da5b098aa340fd
Nick <nick@zipcodes-mbp-78.myfiosgateway.com>
2021-07-15 22:08:19 -0400

    (feat:black-jack) generating tests

--- src/test/java/com/github/zipcodewilmington/BlackJackGameTest.java
+++ src/test/java/com/github/zipcodewilmington/BlackJackGameTest.java
@@ -1,21 +1,21 @@
 package com.github.zipcodewilmington;
 
+import com.github.zipcodewilmington.casino.CasinoAccount;
 import com.github.zipcodewilmington.casino.Player;
 import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.casino.games.Beetle.BeetleGame;
 import com.github.zipcodewilmington.casino.games.blackjack.BlackJack;
 import com.github.zipcodewilmington.casino.games.blackjack.BlackJackGame;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class BlackJackGameTest {
 
-    @Test
-    public void runTest () {
-        //Player player = new Player("Roger", 5000);
-    }
+
     public void startGameTest () {
         BlackJack bj = new BlackJack();
         BlackJackGame blackJackGame = new BlackJackGame();
@@ -49,9 +49,51 @@ public class BlackJackGameTest {
 //        Assert.assertEquals(expected, actual);
 //    }
 
+    @Test
+    public void subtractTest() {
+        BlackJackGame bj = new BlackJackGame();
+        CasinoAccount account = new CasinoAccount("Bjork", "beeyork");
+        PlayerInterface player = new Player("Bjork", account);
+        player.getArcadeAccount().alterAccountBalance(500);
+        bj.add(player);
+        player.getArcadeAccount().alterAccountBalance(200 * -1);
+        Integer expected = 300;
+        Integer actual = player.getArcadeAccount().getAccountBalance();
+        Assert.assertEquals(expected, actual);
+    }
 
     @Test
-    public void splitPlayerHitsBlackJack () {
+    public void splitPlayerHitsBlackJackTest () {
+        BlackJackGame bjg = new BlackJackGame();
+        BlackJack bj = new BlackJack();
         
+    }
+
+    @Test
+    public void runTest () {
+        BlackJackGame bj = new BlackJackGame();
+        bj.setRunning(true);
+        bj.run();
+
+        Integer actual = bj.getUserBet();
+        Integer expected = null;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void printWelcomeTest () {
+        BlackJackGame bj = new BlackJackGame();
+        bj.setRunning(true);
+        bj.run();
+
+        System.out.println("============================================================" + "\n" +
+                "=====                                                  =====" + "\n" +
+                "=====                     WELCOME                      =====" + "\n" +
+                "=====                       TO                         =====" + "\n" +
+                "=====                    B L A C K                     =====" + "\n" +
+                "=====                     J A C K                      =====" + "\n" +
+                "=====                                                  =====" + "\n" +
+                "============================================================");
     }
 }
