7fd3c87b0795c1de8c21a07faeaedca0f706db04
ZachSinger <32113115+ZachSinger@users.noreply.github.com>
2021-07-17 17:20:43 -0400

    Feature/score print (#97)
    * Added wait feature to console that will allow for the console to pause a specified amount of time
    
    * leaving branch, added class Scoreboard, no code on it at this point. Switching to new branch after commit
    
    * Added Scoreboard, GameScoreboard and GameScoreboardInterface, 100% lines and methods tested
    
    * Created CSVArray function on scoreboard to allow for all values in scoreboard to be saved and loaded through the regular account save/load functions. Added Scoreboard to CasinoAccount
    
    Co-authored-by: Zach <zach@zipcodes-MacBook-Pro-6.local>

--- src/main/java/com/github/zipcodewilmington/casino/GameScoreboardInterface.java
+++ src/main/java/com/github/zipcodewilmington/casino/GameScoreboardInterface.java
@@ -1,4 +1,13 @@
 package com.github.zipcodewilmington.casino;
 
+
+import com.github.zipcodewilmington.utils.GameScoreBoard;
+
 public interface GameScoreboardInterface {
+    Integer getLifetimeBets();
+    Integer getLifetimeWinnings();
+    Integer getLifetimeLosses();
+    void addToLifetimeWinnings(Integer winnings);
+    void addToLifetimeLosses(Integer losses);
+    void addToLifetimeBets(Integer bets);
 }
