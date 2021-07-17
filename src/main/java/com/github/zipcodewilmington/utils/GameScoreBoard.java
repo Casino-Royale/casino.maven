22c5c34b5d574de79c46b119563baf2e40f03aef
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-07-17 15:28:00 -0400

    Added Scoreboard, GameScoreboard and GameScoreboardInterface, 100% lines and methods tested

---
+++ src/main/java/com/github/zipcodewilmington/utils/GameScoreBoard.java
@@ -0,0 +1,38 @@
+package com.github.zipcodewilmington.utils;
+
+import com.github.zipcodewilmington.casino.GameScoreboardInterface;
+
+public class GameScoreBoard implements GameScoreboardInterface {
+    private Integer lifetimeBets = 0;
+    private Integer lifetimeWinnings = 0;
+    private Integer lifetimeLosses = 0;
+    private String gameName;
+
+    public GameScoreBoard(String name){
+        this.gameName = name;
+    }
+
+    public Integer getLifetimeBets() {
+        return lifetimeBets;
+    }
+
+    public void incrementLifetimeBets() {
+        this.lifetimeBets++;
+    }
+
+    public Integer getLifetimeWinnings() {
+        return lifetimeWinnings;
+    }
+
+    public void addToLifetimeWinnings(Integer winnings) {
+        this.lifetimeWinnings += winnings;
+    }
+
+    public Integer getLifetimeLosses() {
+        return lifetimeLosses;
+    }
+
+    public void addToLifetimeLosses(Integer losses) {
+        this.lifetimeLosses += losses;
+    }
+}
